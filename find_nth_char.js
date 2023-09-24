c513c5431729a97fee1d9b65892f431db85822e4
ranjith <agrileaf.ranjith@gmail.com>
2023-09-24 22:12:39 +0530

    find Nth letter in string

---
+++ find_nth_char.js
@@ -0,0 +1,13 @@
+//  Bracket Notation to Find Nth Character in String
+//Example
+var demo = "Rtencez";
+var secondLetterofDemo = demo[1];
+
+console.log(secondLetterofDemo);
+
+//setup
+
+var letter = "pocket parfum";
+var seventhLetterofLetter = letter[7];
+
+console.log(seventhLetterofLetter);
