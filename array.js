ed17d5e55c687812ce4c00df3d078b4b051ce872
ranjith <agrileaf.ranjith@gmail.com>
2023-09-25 00:17:21 +0530

    Access Multi-Dimensional Arrays

--- array.js
+++ array.js
@@ -3,12 +3,6 @@
 // Example: Array
 var test = ["car", 18];
 
-//Example : Nested Array
-
-var nestedArray = [ ["car", 12], ["bike", 3]]
-
-console.log(nestedArray);
-
 //Example : Acces array data with Indexes
 var ourArray = [12,23,43,21];
 
---
+++ multiDimentionalArray.js
@@ -0,0 +1,10 @@
+//Example : Nested Array or Multi-Dimensional Arrays
+var nestedArray = [ ["car", 12], ["bike", 3]]
+
+console.log(nestedArray);
+
+//Example : Access Multi-Dimensional Arrays
+var ourArray = [[1,2,3,4], [4,3,12,1], [[7,54,32,33], 23,34]];
+
+var myData = ourArray[2][1];
+console.log(myData);
