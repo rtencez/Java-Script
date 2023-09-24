b3fe69ce502f2dc728876451d8dace6c6d3b7913
ranjith <agrileaf.ranjith@gmail.com>
2023-09-24 22:59:05 +0530

    Word Blanks

--- find_char.js
+++ find_char.js
@@ -1,9 +1,14 @@
 //  Bracket Notation to Find Nth Character in String
-//Example
+//Example : to find Nth letter of string 
 var demo = "Rtencez";
 var secondLetterofDemo = demo[1];
 
+//Example : to find last letter of string 
+var demo1 = "wednesday";
+var lastLetterOfDemo1 = demo1[demo1.length - 1];
+
 console.log(secondLetterofDemo);
+console.log(lastLetterOfDemo1);
 
 //setup
 
---
+++ word_blanks.js
@@ -0,0 +1,9 @@
+function wordBlanks(word1, word2, word3, word4){
+    var result =" ";
+
+    result += "Haii there"+" "+ word1 + " " + "this" +" "+ "is" +" "+ word2 + " " + word3 + " " + word4
+
+    return result;
+}
+
+console.log(wordBlanks("welcome back","mr","beast","lets go"));
