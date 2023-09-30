69bdf22208b15326fad452c2d445bcb42b18cf0e
ranjith <agrileaf.ranjith@gmail.com>
2023-09-30 22:27:50 +0530

    Array push method

---
+++ manipulate_array.js
@@ -0,0 +1,13 @@
+//Example : Push()  data added to end of the array
+
+var myArray = [12,21,21,432];
+// Array before push :  [ 12, 21, 21, 432 ]
+
+myArray.push(['simpson','noland']);
+console.log('Array after push :',myArray);
+
+// Example 2 
+var exa = [12,'good','note','yagami',[87,34,65,23]];
+exa.push([82,'added', 'Array'])
+
+console.log(exa);
