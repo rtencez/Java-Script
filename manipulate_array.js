d1da8d4e1d3c2f20f5fa297457caf01a4e1fec7d
ranjith <agrileaf.ranjith@gmail.com>
2023-09-30 22:52:04 +0530

    pop method

--- manipulate_array.js
+++ manipulate_array.js
@@ -1,4 +1,4 @@
-//Example : Push()  data added to end of the array
+//Push() method : Data added to end of the array
 
 var myArray = [12,21,21,432];
 // Array before push :  [ 12, 21, 21, 432 ]
@@ -11,3 +11,16 @@ var exa = [12,'good','note','yagami',[87,34,65,23]];
 exa.push([82,'added', 'Array'])
 
 console.log(exa);
+
+// Pop() method : Remove item from end of the array
+
+var removeArray = [1,3,4,'kjhbbm']
+removeArray.pop();
+
+console.log(removeArray);
+
+//Example 2 
+var carPart = ['tire','engine','petrol', ['seat','baterry']];
+carPart.pop();
+
+console.log(carPart);
