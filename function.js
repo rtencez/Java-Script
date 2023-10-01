b9102968cb51ba9375bd4172150321806b461a92
ranjith <agrileaf.ranjith@gmail.com>
2023-10-01 17:52:35 +0530

    functions

---
+++ function.js
@@ -0,0 +1,13 @@
+function newFunction() {
+    console.log('hello world');
+}
+
+newFunction();
+newFunction();
+newFunction();
+
+function reUsableFunction() {
+    console.log('hello mr');
+}
+
+reUsableFunction();
