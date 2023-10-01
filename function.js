6e27b9f8ecbac46126cc8a4e3a142c5c0de16aee
ranjith <agrileaf.ranjith@gmail.com>
2023-10-01 19:45:48 +0530

    functions

--- function.js
+++ function.js
@@ -1,3 +1,6 @@
+// A JavaScript function is a block of code designed to perform a particular task and allows reusability of code.
+// execute when it called
+
 function newFunction() {
     console.log('hello world');
 }
