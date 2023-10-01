51a12aac599a0f5099a779ced909ba7fa5260c17
ranjith <agrileaf.ranjith@gmail.com>
2023-10-01 19:46:30 +0530

    function with arguments

---
+++ function_with_agrument.js
@@ -0,0 +1,16 @@
+// Passing Values to Function with Agrumenst
+
+function sumOfTwoNumber(a, b) {
+    console.log(a+b);
+}
+
+sumOfTwoNumber(2,5);
+sumOfTwoNumber(32,53);
+
+// Example 2 :
+
+function findReminder(num1, num2) {
+    console.log(num1 % num2);
+}
+
+findReminder(21,240);
