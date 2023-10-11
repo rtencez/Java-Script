3fd27f20d44fbcd57fcc1781e26e75001968938a
ranjith <agrileaf.ranjith@gmail.com>
2023-10-11 06:45:49 +0530

    Undefined Value in function

---
+++ undefined_value.js
@@ -0,0 +1,18 @@
+// Undefined Value returned when there is no `return` in function
+var sum  = 0;
+
+function result() {
+    sum *=2;
+}
+
+// console.log(result); // undefined
+
+//Example 2 :
+function addFive() {
+    sum = sum + 5
+}
+
+// console.log(addFive);
+
+
+
