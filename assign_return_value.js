38d713b905e53111c9339aebfb9941421063561f
ranjith <agrileaf.ranjith@gmail.com>
2023-10-11 07:17:13 +0530

    practice on array

---
+++ assign_return_value.js
@@ -0,0 +1,17 @@
+// Assign with a Returned Value
+
+var changed = 0;
+
+function change(num) {
+    return (num+5)/3;
+}
+
+changed = change(7);
+
+var result = 0;
+
+function sumofTwoDigit(num1, num2) {
+    return num1 + num2;
+}
+
+result = sumofTwoDigit(67,7);
---
+++ practice1.js
@@ -0,0 +1,18 @@
+/*
+testArr = [1,2,3,4,5,6,7];
+
+remove 1st item in array and add new item to end of the index using function
+and display array before and after
+
+ */
+
+function arrayAction(arr , item){
+    arr.push(item);
+    arr.shift();
+}
+
+var testArr=[1,2,3,4,5,6,7]
+
+console.log("Before :" + JSON.stringify(testArr));
+console.log(arrayAction(testArr, 9));
+console.log("After :" +JSON.stringify(testArr));
