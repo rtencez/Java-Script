33b14290bc2f5595a5f749cc864fd18ef40a48cc
ranjith <agrileaf.ranjith@gmail.com>
2023-10-11 06:28:07 +0530

    Return a Value from a Function

---
+++ return_value.js
@@ -0,0 +1,14 @@
+// Return a Value from a Function
+function addNum(num) {
+    return num + 20; 
+}
+
+console.log(addNum(10));
+
+//Example 2:
+
+function squareNum(num) {
+    return num * num;
+}
+
+console.log(squareNum(9));
