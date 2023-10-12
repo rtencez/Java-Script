18af1cf088a1b700cf9d8c7931cbe90f281a8abc
ranjith <agrileaf.ranjith@gmail.com>
2023-10-12 07:04:29 +0530

    comparition with `==` Equality Operators

---
+++ equality_Operators.js
@@ -0,0 +1,12 @@
+/*
+`=` assignment operator
+`==` comparotion operator
+*/
+
+function testEqual(val) {
+    if(val==12){
+        return "Equal";
+    }
+    return "Not Equal"
+}
+console.log(testEqual(12));
