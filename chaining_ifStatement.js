6bb535be80c2336f3c12ef71d025a601f044a6a9
ranjith <agrileaf.ranjith@gmail.com>
2023-11-04 18:11:08 +0530

    Chaining If Else Statements

---
+++ chaining_ifStatement.js
@@ -0,0 +1,25 @@
+/*
+ * Write chanined if statement to fulfill the following conditions:
+ * 
+ * num <5 - return "Tiny"
+ * num <10 - return "Small"
+ * num < 15 - return "Medium"
+ * num < 20 - return "Large"
+ * num >= 20 - return "Huge"
+ * 
+ */
+
+function testSize(num) {
+    if (num < 5) {
+        return "Tiny"
+    } else if (num < 10) {
+        return "Small"
+    } else if (num < 15) {
+        return "Medium" 
+    } else if (num < 20) {
+        return "Large"
+    } else {
+        return "Huge"
+    } 
+}
+console.log(testSize(6));
