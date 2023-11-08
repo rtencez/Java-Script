639f2167f0a4f41dc43ade45dfefe1b8d5b9c677
ranjith <agrileaf.ranjith@gmail.com>
2023-11-08 07:24:26 +0530

    Counting Cards

---
+++ counting_card.js
@@ -0,0 +1,34 @@
+var count = 0;
+
+function cc(card) {
+
+    switch (card) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count++;
+            break;
+
+
+        case 10:
+        case 'K':
+        case 'J':
+        case 'A':
+        case 'Q':
+            count--;
+            break;
+    }
+
+    var holdbet = 'Hold'
+
+    if (count >0){
+        holdbet = 'Bet'
+    }
+    return count+ " " + holdbet;
+}
+
+cc(2); cc(3); cc('K'); cc('A'); cc('A');
+
+console.log(cc(8));
