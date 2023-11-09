f45182bb7d1e6042b804bb7f4efcd45957c34d7c
ranjith <agrileaf.ranjith@gmail.com>
2023-11-09 06:30:11 +0530

    objects in js

---
+++ objects.js
@@ -0,0 +1,11 @@
+/**
+ * Similar to array, More info: https://www.w3schools.com/js/js_object_definition.asp
+ */
+ 
+var school = {
+    "name": schoolName,
+    "address": address,
+    "students": [student1, student2], // Array of students in the school
+};
+
+//
