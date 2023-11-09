dc4e105ec39fa71933531aaae9245a34f6096be0
ranjith <agrileaf.ranjith@gmail.com>
2023-11-09 06:39:28 +0530

    access object value using dot notation

--- objects.js
+++ objects.js
@@ -1,11 +1,20 @@
 /**
  * Similar to array, More info: https://www.w3schools.com/js/js_object_definition.asp
+ *              <datatype> objectName ={
+ *                          "property": value;             
+ *               };
  */
  
 var school = {
-    "name": schoolName,
-    "address": address,
-    "students": [student1, student2], // Array of students in the school
+    "name": "schoolName",
+    "address": "address",
+    "students": ["student!","student2"], // Array of students in the school
 };
 
-//
+//Access property in Objects using dot notation
+
+var nameValue = school.name;
+var studentsValue = school.students;
+
+console.log(nameValue);
+console.log(studentsValue);
