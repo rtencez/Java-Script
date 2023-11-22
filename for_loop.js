781e97a6100a8e6c02111fa4b97b714f546874fe
ranjith <agrileaf.ranjith@gmail.com>
2023-11-22 07:33:24 +0530

    for loop

---
+++ for_loop.js
@@ -0,0 +1,17 @@
+//  keyword used : for
+// General form of for loop:
+// for(initialization; condition; finalExprestion) {
+// 
+// }
+ 
+var ourArray = [];
+
+for (var i =0; i <5; i++) {
+    ourArray.push(i);
+}
+console.log(ourArray);
+
+for (var i = 1; i < 6; i++){
+    ourArray.push(i);
+}
+console.log(ourArray);
