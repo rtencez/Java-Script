ee04252b54eed6ac3235fb0e7e82c57f6627151b
ranjith <agrileaf.ranjith@gmail.com>
2023-11-25 06:51:09 +0530

    for loop

--- for_loop.js
+++ for_loop.js
@@ -9,9 +9,11 @@ var ourArray = [];
 for (var i =0; i <5; i++) {
     ourArray.push(i);
 }
-console.log(ourArray);
+console.log(ourArray); 
 
 for (var i = 1; i < 6; i++){
     ourArray.push(i);
 }
 console.log(ourArray);
+
+// Count backward with for loop
