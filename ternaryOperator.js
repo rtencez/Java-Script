9ecdc8e2c15311c282dad307afb5adf092809d6a
ranjith <agrileaf.ranjith@gmail.com>
2023-11-28 06:46:17 +0530

    ternary operator

---
+++ ternaryOperator.js
@@ -0,0 +1,13 @@
+  /**
+   * Ternary operator is like single line if-else statement
+   * general form // condition ? statement-if-true : statement-if-false;
+   * 
+   */
+    
+  function checkEqual(a,b) {
+    return a==b ? true:false;
+
+    return a==b;
+
+  }
+  console.log(checkEqual(1,2));
