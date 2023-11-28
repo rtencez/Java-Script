302bb43ff1423878aa866b7e8a81d4993197d675
ranjith <agrileaf.ranjith@gmail.com>
2023-11-28 06:17:53 +0530

    Generate Random whole number

---
+++ generate_RandomWholeNumber.js
@@ -0,0 +1,10 @@
+/**
+ * Generate Random whole number With range,
+ *  Math.floor(Math.random() *20); it generate random number between 0 to 19
+ */
+var radomWholeNumBtw0and19 = Math.floor(Math.random() *20);
+
+function wholeNumber(){
+    return Math.random(Math.random()* 10);
+}
+
